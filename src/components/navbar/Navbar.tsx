9f96907146e29f4f1c5b40081746c35e9b9be4ea
Mireli Borges <mika.oliveira1998@gmail.com>
2025-04-22 15:53:22 -0300

    Parte de home, navbar e footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-blue-800 text-white'>
+            
+                <div className="container flex justify-between font-semibold">
+                <Link to='/home' className="text-2xl font-semibold">Convênio Lorenzo</Link>
+
+                    <div className='flex gap-4'>
+                        Convenios 
+                        Coberturas 
+                        Cadastrar 
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
